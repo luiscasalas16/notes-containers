5043e2abd05f0d2fa63bf683100489b5460afdd0
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2023-08-21 13:48:59 -0600

    ejemplo

---
+++ net/NetDockerConsoleWindows64/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace NetDockerConsoleWindows64
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("NetDockerConsoleWindows64");
+            var counter = 0;
+            var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max is -1 || counter < max)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
